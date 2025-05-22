b95126e8e7cfe0e37b8523c731991da2bef7579c
unorderlyhamster329 <118472366+unorderlyhamster329@users.noreply.github.com>
2025-05-22 18:22:30 +0000

    eqeeweqw

--- src/jfo-section-1/HelloWorld.java
+++ src/jfo-section-1/HelloWorld.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        long bignumber = 9999999;
+        short shortington = 255;
+        byte bytington = 42;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("whats ur grade");
+        int grade = input.nextInt();
+        System.out.println("ur grade next year is: " + (grade + 1));
+        input.close();
     }
 }
--- src/jfo-section-2/Text01.java
+++ src/jfo-section-2/Text01.java
@@ -1,5 +1,8 @@
 public class Text01 {
+
+    
     public static void main(String[] args) {
+
         System.out.println("   /\\         /\\   ");
         System.out.println("  /  \\_______/  \\  ");
         System.out.println(" /               \\ ");
